73b638e6bb4744de267353669c938d6f704a1148
Alexandr Romanov <aromanov07@gmail.com>
2022-11-19 14:03:25 +0300

    Add first appear

--- Sources/OversizeCore/ViewModifier/OnFirstAppear.swift
+++ Sources/OversizeCore/ViewModifier/OnFirstAppear.swift
@@ -7,9 +7,9 @@ import SwiftUI
 
 private struct FirstAppear: ViewModifier {
     let action: () -> Void
-    
+
     @State private var hasAppeared = false
-    
+
     func body(content: Content) -> some View {
         content.onAppear {
             guard !hasAppeared else { return }
