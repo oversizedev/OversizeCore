e636898683c9ac5be68333886f78fb3583ecbb64
Alexander Romanov <aromanov07@gmail.com>
2023-03-14 22:41:12 +0300

    Small refactor

--- Sources/OversizeCore/Extensions/Swift/String+Extension.swift
+++ Sources/OversizeCore/Extensions/Swift/String+Extension.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2022 Alexander Romanov
+// Copyright © 2023 Alexander Romanov
 // String+Extension.swift
 //
 
@@ -14,27 +14,21 @@ public extension String {
     }
 
     var isEmail: Bool {
-        get {
-            let regEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
-            let predicate: NSPredicate = .init(format: "SELF MATCHES %@", regEx)
-            return predicate.evaluate(with: self)
-        }
+        let regEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+        let predicate: NSPredicate = .init(format: "SELF MATCHES %@", regEx)
+        return predicate.evaluate(with: self)
     }
-    
+
     var isURL: Bool {
-        get {
-            let regEx = "http[s]?://(([^/:.[:space:]]+(.[^/:.[:space:]]+)*)|([0-9](.[0-9]{3})))(:[0-9]+)?((/[^?#[:space:]]+)([^#[:space:]]+)?(#.+)?)?"
-            let predicate = NSPredicate(format: "SELF MATCHES %@", argumentArray: [regEx])
-            return predicate.evaluate(with: self)
-        }
+        let regEx = "http[s]?://(([^/:.[:space:]]+(.[^/:.[:space:]]+)*)|([0-9](.[0-9]{3})))(:[0-9]+)?((/[^?#[:space:]]+)([^#[:space:]]+)?(#.+)?)?"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", argumentArray: [regEx])
+        return predicate.evaluate(with: self)
     }
 
     var isAlphabet: Bool {
-        get {
-            let regEx = "[A-Za-z_]*"
-            let predicate: NSPredicate = .init(format: "SELF MATCHES %@", regEx)
-            return predicate.evaluate(with: self)
-        }
+        let regEx = "[A-Za-z_]*"
+        let predicate: NSPredicate = .init(format: "SELF MATCHES %@", regEx)
+        return predicate.evaluate(with: self)
     }
 
     var urlEncode: String {
@@ -44,21 +38,17 @@ public extension String {
     }
 
     var url: URL? {
-        get {
-            var URLString = self
-            if URLString.hasPrefix("//") {
-                URLString = "http:" + URLString
-                if let URL = URL(string: URLString) {
-                    return URL
-                }
-            } else if let URL = URL(string: URLString) {
+        var URLString = self
+        if URLString.hasPrefix("//") {
+            URLString = "http:" + URLString
+            if let URL = URL(string: URLString) {
                 return URL
             }
-            return nil
+        } else if let URL = URL(string: URLString) {
+            return URL
         }
+        return nil
     }
-    
-
 
     var localizedDecimalSeparator: String {
         let nf: NumberFormatter = .init()
