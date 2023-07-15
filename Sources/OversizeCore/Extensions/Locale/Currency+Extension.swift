59707da38e7fe0b08fad62710a9e56dbe415e6e1
Alexander Romanov <aromanov07@gmail.com>
2023-07-15 18:04:33 +0300

    Add Locale extensions

---
+++ Sources/OversizeCore/Extensions/Locale/Currency+Extension.swift
@@ -0,0 +1,34 @@
+//
+// Copyright © 2023 Alexander Romanov
+// CurrencyExtension.swift, created on 15.07.2023
+//
+
+import Foundation
+
+@available(macOS 13, iOS 16, tvOS 16, watchOS 9, *)
+public extension Locale.Currency {
+    var dispalyName: String? {
+        Locale.current.localizedString(forCurrencyCode: identifier)
+    }
+
+    var dispalyLocalizedSymbol: String? {
+        Locale.current.localizedCurrencySymbol(forCurrencyCode: identifier)
+    }
+
+    var dispalySymbol: String? {
+        let allLocalCurrencies = Locale.availableIdentifiers.compactMap { Locale(identifier: $0) }
+        return allLocalCurrencies.first { $0.currency == self }?.currencySymbol
+    }
+
+    var locale: Locale? {
+        let allLocalCurrencies = Locale.availableIdentifiers.compactMap { Locale(identifier: $0) }
+        return allLocalCurrencies.first { $0.currency == self }
+    }
+
+    static var countryCurrencies: [Locale.Currency] {
+        Locale.availableIdentifiers
+            .compactMap { Locale(identifier: $0).currency }
+            .removeDuplicates()
+            .sorted { $0.dispalyName ?? "" < $1.dispalyName ?? "" }
+    }
+}
---
+++ Sources/OversizeCore/Extensions/Locale/Locale+Extension.swift
@@ -0,0 +1,23 @@
+//
+// Copyright © 2023 Alexander Romanov
+// LocaleExtension.swift, created on 15.07.2023
+//
+
+import Foundation
+
+@available(macOS 13, iOS 16, tvOS 16, watchOS 9, *)
+extension Locale {
+    func localizedCurrencySymbol(forCurrencyCode currencyCode: String) -> String? {
+        guard
+            let languageCode = language.languageCode?.identifier,
+            let regionCode = language.region?.identifier
+        else { return nil }
+        let components: [String: String] = [
+            NSLocale.Key.languageCode.rawValue: languageCode,
+            NSLocale.Key.countryCode.rawValue: regionCode,
+            NSLocale.Key.currencyCode.rawValue: currencyCode,
+        ]
+        let identifier = Locale.identifier(fromComponents: components)
+        return Locale(identifier: identifier).currencySymbol
+    }
+}
