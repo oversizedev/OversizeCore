ae9f49638099d2b769179bc6f11ce3d37b0e2243
Alexander Romanov <aromanov07@gmail.com>
2026-04-28 20:52:49 +0300

    Fix liunux

--- Sources/OversizeCore/Extensions/URL/URLCache+Extension.swift
+++ Sources/OversizeCore/Extensions/URL/URLCache+Extension.swift
@@ -4,7 +4,10 @@
 //
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
 
 public extension URLCache {
     static let imageCache = URLCache(memoryCapacity: 512_000_000, diskCapacity: 1_000_000_000)
 }
+#endif
