84111b682f95c74624d0a042ed280103040a5a1c
Alexander Romanov <aromanov07@gmail.com>
2026-04-28 20:56:51 +0300

    Fix Linux

--- Sources/OversizeCore/Extensions/URL/URLCache+Extension.swift
+++ Sources/OversizeCore/Extensions/URL/URLCache+Extension.swift
@@ -4,9 +4,8 @@
 //
 
 import Foundation
-#if canImport(FoundationNetworking)
-import FoundationNetworking
 
+#if os(iOS)
 public extension URLCache {
     static let imageCache = URLCache(memoryCapacity: 512_000_000, diskCapacity: 1_000_000_000)
 }
